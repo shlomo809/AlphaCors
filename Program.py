92c360332e75d1a8acb1c02d319d1dd584c7fe5b
Shlomo <shlomo809@gmail.com>
2021-10-03 18:42:15 +0300

    aaa

--- Program.py
+++ Program.py
@@ -1,9 +1,30 @@
 def main():
+    my_num=[5,6,7,14,152,60693]
     num =0
     for i in range(100): 
         num = num + i + 1
+        if(i < 6 ):
+            
+            d = IsPrimeNumber(my_num[i])
+            print(d)
+            
         
-
+        
+def IsPrimeNumber(x):
+      
+    
+    
+       
+    
+        for j in range(2,x-1):
+           
+            if(x % j == 0):
+               
+                return False
+            return True   
+        
+            
+           
     
 
 
